8f12aee210d115fe04c4395f77a99547b8707993
rodriguesprado.felipe <rodriguesprado.felipe@gmail.com>
2013-10-15 13:03:36 +0000

    Mudança na senha do banco de dados.

--- financialcontrolsystem/src/financialcontrolsystem/model/ConnectionDatabase.java
+++ financialcontrolsystem/src/financialcontrolsystem/model/ConnectionDatabase.java
@@ -12,17 +12,17 @@ public class ConnectionDatabase {
 	public void createConnection() {
 		String url = "jdbc:postgresql://localhost:5432/financialcontrolsystem";
 		String user = "postgres";
-		String password = "1583141";
+		String password = "1583141aehcada";
 
 		try {
 			connection = DriverManager.getConnection(url, user, password);
 			System.out.println("Conexão com banco de dados estabelecida!");
 
-//			String sql = "insert into account (active, description) values (?, ?)";
-//			PreparedStatement statement = connection.prepareStatement(sql);
-//			statement.setBoolean(1, true);
-//			statement.setString(2, "Dinheiro");
-//			statement.executeUpdate();
+			String sql = "insert into account (active, description) values (?, ?)";
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setBoolean(1, true);
+			statement.setString(2, "Dinheiro");
+			statement.executeUpdate();
 
 		} catch (SQLException e) {
 			System.out.println("Erro ao conectar com o banco de dados!");
