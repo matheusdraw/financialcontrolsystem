0f2bd64dfb735537e9bb87be7f98998fcfcd1f76
rodriguesprado.felipe@gmail.com <rodriguesprado.felipe@gmail.com@ba52573c-0155-f6dc-b486-da5973e56fad>
2013-10-22 03:25:16 +0000

    Criação da class que faz a máscara para campos númericos. A class recebe como parâmetros a quantidade de digitos e a quantidade de casas decimais que se deseja.

--- trunk/financialcontrolsystem/src/financialcontrolsystem/view/FormatMaskField.java
+++ trunk/financialcontrolsystem/src/financialcontrolsystem/view/FormatMaskField.java
@@ -1,10 +1,6 @@
 package financialcontrolsystem.view;
 
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.text.ParseException;
-
-import javax.swing.JTextField;
 import javax.swing.text.MaskFormatter;
 import javax.swing.text.PlainDocument;
 
@@ -36,88 +32,5 @@ public class FormatMaskField extends PlainDocument {
 		}
 		return maskDateField;
 	}
-	
-//	public void formatNumericFloat(KeyEvent f, final JTextField x) {
-//		
-//	
-//		
-//		class MyKeyListener implements KeyListener {
-//
-//			@Override
-//			public void keyPressed(KeyEvent e) {
-//				JTextField fieldInitialValue = x;
-//									
-//				if (fieldInitialValue.getText().length() > 10) {
-//					e.consume();
-//				}
-//				
-//			}
-//
-//			@Override
-//			public void keyReleased(KeyEvent e) {
-//				JTextField fieldInitialValue = x;
-//				//fieldInitialValue.setText(fieldInitialValue.getText().replaceAll("[^0-9 && ,.]", ""));
-//				
-//				StringBuffer stringBuffer = new StringBuffer(fieldInitialValue.getText());
-//				String texto = fieldInitialValue.getText();
-//				texto.replaceAll("[^0-9 && ,.]", "");
-//				fieldInitialValue.setText(texto);
-//				
-//				if (stringBuffer.length() == 2) {
-//					stringBuffer.insert(1, ",");
-//					fieldInitialValue.setText(stringBuffer.toString());
-//				} else if (stringBuffer.length() == 3) {
-//					stringBuffer.insert(1, ",");
-//					fieldInitialValue.setText(stringBuffer.toString());
-//				} else if (stringBuffer.length() == 4) {
-//					stringBuffer.insert(2, ",");
-//					fieldInitialValue.setText(stringBuffer.toString());
-//				} else if (stringBuffer.length() == 5) {
-//					stringBuffer.insert(3, ",");
-//					fieldInitialValue.setText(stringBuffer.toString());
-//				} else if (stringBuffer.length() == 6) {
-//					stringBuffer.insert(1, ".");
-//					stringBuffer.insert(5, ",");
-//					fieldInitialValue.setText(stringBuffer.toString());
-//				} else if (stringBuffer.length() == 7) {
-//					stringBuffer.insert(2, ".");
-//					stringBuffer.insert(6, ",");
-//					fieldInitialValue.setText(stringBuffer.toString());
-//				} else if (stringBuffer.length() == 8) {
-//					stringBuffer.insert(3, ".");
-//					stringBuffer.insert(7, ",");
-//					fieldInitialValue.setText(stringBuffer.toString());
-//				} else if (stringBuffer.length() == 9) {
-//					stringBuffer.insert(1, ".");
-//					stringBuffer.insert(5, ".");
-//					stringBuffer.insert(9, ",");
-//					fieldInitialValue.setText(stringBuffer.toString());
-//				} else if (stringBuffer.length() == 10) {
-//					stringBuffer.insert(2, ".");
-//					stringBuffer.insert(6, ".");
-//					stringBuffer.insert(10, ",");
-//					fieldInitialValue.setText(stringBuffer.toString());
-//				} else if (stringBuffer.length() == 11) {
-//					stringBuffer.insert(3, ".");
-//					stringBuffer.insert(7, ".");
-//					stringBuffer.insert(11, ",");
-//					fieldInitialValue.setText(stringBuffer.toString());
-//				}
-//				
-//			}
-//
-//			@Override
-//			public void keyTyped(KeyEvent e) {
-//				// TODO Auto-generated method stub
-//				
-//			}
-//			
-//			
-//		}
-//		MyKeyListener m = new MyKeyListener();
-//		m.keyPressed(f);
-//		m.keyReleased(f);
-//		
-//	}
 
 }
--- trunk/financialcontrolsystem/src/financialcontrolsystem/view/FormatNumericField.java
+++ trunk/financialcontrolsystem/src/financialcontrolsystem/view/FormatNumericField.java
@@ -6,27 +6,98 @@ import javax.swing.text.PlainDocument;
 
 public class FormatNumericField extends PlainDocument {
 
-	@Override
-	public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
-		
-		// getText = TRAZ O QUE TEM NO CAMPO JTextField
-		// str = É O QUE FOI DIGITADO MAS NÃO INSERIDO NO CAMPO ATÉ CHAMAR super.insertString(offs, str, a);		
-				
-		StringBuffer content = new StringBuffer(getText(0, getLength()) + str);
-		
-		if (content.length() > 5) {
-			return;
-		}
-		
-		String contentOk = content.toString();
-		contentOk = contentOk.replaceAll("a", "");
-		
-		System.out.println("O que foi digitado: " + str);		
-		System.out.println("O que tem no campo: "+ getText(0, getLength()));
-		System.out.println("O que tem no campo + o que foi digitado: " + content);
-		System.out.println("Valor final: " + contentOk);
-		
-		super.insertString(offs, str, a);
+	// getText = TRAZ O QUE TEM NO CAMPO JTextField
+	// str = É O QUE FOI DIGITADO MAS NÃO INSERIDO NO CAMPO ATÉ CHAMAR
+	// super.insertString(offs, str, a);
+
+	private static final long serialVersionUID = 1L;
+	private int size;
+	private int decimals;
+	private int digits;
+
+	public FormatNumericField(int size, int decimals) {
+		this.size = size;
+		this.decimals = decimals;
 	}
-	
+
+	@Override
+	public void insertString(int offs, String str, AttributeSet a)
+			throws BadLocationException {
+
+//		if (getLength() == 0) {
+//			str = str.replaceAll("[^0-9 || -]", "");
+//		} else {
+//			str = str.replaceAll("[^0-9]", "");
+//		}
+		
+		str = str.replaceAll("[^0-9]", "");
+
+		StringBuffer content = new StringBuffer(getText(0, getLength()) + str);
+
+		if (content.length() > decimals) {
+
+			str = content.toString().replaceAll("[, || .]", "");
+
+			if (str.length() > size + decimals) {
+				return;
+			}
+
+			content = new StringBuffer(str);
+
+			digits = content.length() - decimals;
+
+			if (digits >= 1 && digits <= 3) {
+				content.insert(digits, ",");
+				super.remove(0, getLength());
+				super.insertString(0, content.toString(), a);
+				return;
+			}
+
+			if (digits > 3 && digits <= 6) {
+				content.insert(digits, ",");
+				content.insert(digits - 3, ".");
+				super.remove(0, getLength());
+				super.insertString(0, content.toString(), a);
+				return;
+			}
+
+			if (digits > 6 && digits <= 9) {
+				content.insert(digits, ",");
+				content.insert((digits - 6) + 3, ".");
+				content.insert(digits - 6, ".");
+				super.remove(0, getLength());
+				super.insertString(0, content.toString(), a);
+				return;
+			}
+
+			if (digits > 9 && digits <= 12) {
+				content.insert(digits, ",");
+				content.insert((digits - 9) + 6, ".");
+				content.insert((digits - 9) + 3, ".");
+				content.insert(digits - 9, ".");
+				super.remove(0, getLength());
+				super.insertString(0, content.toString(), a);
+				return;
+			}
+
+			if (digits > 12 && digits <= 15) {
+				content.insert(digits, ",");
+				content.insert((digits - 12) + 9, ".");
+				content.insert((digits - 12) + 6, ".");
+				content.insert((digits - 12) + 3, ".");
+				content.insert(digits - 12, ".");
+				super.remove(0, getLength());
+				super.insertString(0, content.toString(), a);
+				return;
+			}
+
+		} else
+			super.insertString(offs, str, a);
+	}
+
+	@Override
+	public void remove(int offs, int len) throws BadLocationException {
+		super.remove(offs, len);
+		insertString(getLength(), "", null);
+	}
 }
